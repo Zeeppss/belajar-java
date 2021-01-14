b891de1354605bc62b71fc79b6cee15a7fce56a5
Varadayy <54202912+Varadayy@users.noreply.github.com>
2021-01-14 08:07:47 +0700

    Add files via upload
    ---- classes ----
    [+] Enums
    [+] User Input
    [+] Date
    [+] ArrayList

---
+++ src/dev/varaday/belajarjava/classes/ArrayLists.java
@@ -0,0 +1,144 @@
+package dev.varaday.belajarjava.classes;
+
+/* Kelas ArrayList adalah array yang dapat diubah ukurannya, yang dapat ditemukan di paket java.util.
+Perbedaan antara array built-in dan ArrayList di Java, adalah bahwa ukuran array tidak dapat diubah (jika Anda ingin menambah atau menghapus elemen ke / dari array, Anda harus membuat yang baru).
+Sementara elemen dapat ditambahkan dan dihapus dari ArrayList kapan pun Anda mau. Sintaksnya juga sedikit berbeda:
+
+Buat objek ArrayList bernama mobil yang akan menyimpan string:
+
+import java.util.ArrayList; // import kelas ArrayList
+
+ArrayList<String> mobil = new ArrayList<String>(); // Membuat objek ArrayList
+ */
+
+// Menambahkan item
+// Kelas ArrayList memiliki banyak metode yang berguna. Misalnya, untuk menambahkan elemen ke ArrayList, gunakan metode add():
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class AL {
+    public static void main(String[] args) {
+        ArrayList<String> mobil = new ArrayList<String>();
+        mobil.add("Avanza");
+        mobil.add("Toyota");
+        mobil.add("Mitsubishi");
+        mobil.add("Alphard");
+        System.out.println(mobil);
+    }
+}
+
+/* Akses Item
+// Untuk mengakses elemen di ArrayList, gunakan metode get() dan lihat nomor indeks:
+mobil.get(0);
+*/
+
+/* Ubah item
+Untuk mengubah sebuah elemen, gunakan metode set() dan lihat nomor indeks:
+mobil.set(0, "Lamborghini");
+ */
+
+/* Hapus Item
+Untuk menghapus elemen, gunakan metode remove() dan merujuk ke nomor indeks:
+mobil.remove(0);
+ */
+
+// Untuk menghapus semua elemen di ArrayList, gunakan metode clear():
+// mobil.clear();
+
+/* Ukuran ArrayList
+Untuk mengetahui berapa banyak elemen yang dimiliki ArrayList, gunakan metode ukuran:
+mobil.size();
+ */
+
+// Ulangi Melalui ArrayList
+// Ulangi elemen ArrayList dengan perulangan for, dan gunakan metode size() untuk menentukan berapa kali perulangan harus dijalankan:
+
+class AL1 {
+    public static void main(String[] args) {
+        ArrayList<String> mobil = new ArrayList<String>();
+        mobil.add("Avanza");
+        mobil.add("Toyota");
+        mobil.add("Mitsubishi");
+        mobil.add("Alphard");
+        for (int i = 0; i < mobil.size(); i++) {
+            System.out.println(mobil.get(i));
+        }
+    }
+}
+
+// Anda juga dapat melakukan loop melalui ArrayList dengan for-each loop:
+
+class AL2 {
+    public static void main(String[] args) {
+        ArrayList<String> mobil = new ArrayList<String>();
+        mobil.add("Avanza");
+        mobil.add("Toyota");
+        mobil.add("Mitsubishi");
+        mobil.add("Alphard");
+        for (String i : mobil) {
+            System.out.println(i);
+        }
+    }
+}
+
+/* Tipe yang lain
+Elemen dalam ArrayList sebenarnya adalah objek.
+Dalam contoh di atas, kami membuat elemen (objek) berjenis "String".
+Ingatlah bahwa String di Java adalah objek (bukan tipe primitif).
+Untuk menggunakan tipe lain, seperti int, Anda harus menentukan kelas pembungkus yang setara: Integer.
+Untuk tipe primitif lainnya, gunakan: Boolean untuk boolean, Karakter untuk char, Double untuk double, dll:
+
+Buat ArrayList untuk menyimpan angka (tambahkan elemen bertipe Integer):
+ */
+
+class AL3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(10);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(25);
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
+    }
+}
+
+/* Sortir ArrayList
+Kelas berguna lainnya dalam paket java.util adalah kelas Koleksi, yang menyertakan metode sort() untuk menyortir daftar menurut abjad atau numerik:
+ */
+
+class AL4 {
+    public static void main(String[] args) {
+        ArrayList<String> mobil = new ArrayList<String>();
+        mobil.add("Avanza");
+        mobil.add("Toyota");
+        mobil.add("Mitsubishi");
+        mobil.add("Alphard");
+        Collections.sort(mobil);  // Sort cars
+        for (String i : mobil) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Sortir ArrayList of Integers:
+
+class AL5 {
+    public static void main(String[] args) {
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+        myNumbers.add(8);
+        myNumbers.add(12);
+
+        Collections.sort(myNumbers);  // sortir myNumbers
+
+        for (int i : myNumbers) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/dev/varaday/belajarjava/classes/Enums.java
@@ -0,0 +1,55 @@
+package dev.varaday.belajarjava.classes;
+
+/* Enum adalah "kelas" khusus yang mewakili sekelompok konstanta (variabel yang tidak dapat diubah, seperti variabel akhir).
+Untuk membuat enum, gunakan kata kunci enum (bukan class atau interface),
+dan pisahkan konstanta dengan koma. Perhatikan bahwa mereka harus dalam huruf besar:
+
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+ */
+
+// Anda dapat mengakses konstanta enum dengan sintaks titik:
+// Level myVar = Level.MEDIUM;
+
+public class Enums {
+    enum Level {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+        System.out.println(myVar);
+    }
+}
+
+// Enum dalam Pernyataan Switch
+// Enum sering digunakan dalam pernyataan sakelar untuk memeriksa nilai yang sesuai:
+
+enum Level {
+    LOW,
+    MEDIUM,
+    HIGH
+}
+
+class Enums1 {
+    public static void main(String[] args) {
+        Level myVar = Level.MEDIUM;
+
+        switch(myVar) {
+            case LOW:
+                System.out.println("Low level");
+                break;
+            case MEDIUM:
+                System.out.println("Medium level");
+                break;
+            case HIGH:
+                System.out.println("High level");
+                break;
+        }
+    }
+}
