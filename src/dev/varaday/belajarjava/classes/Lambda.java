76f475a607f71e991f7468409b461c71fdea5f11
Varadayy <54202912+Varadayy@users.noreply.github.com>
2021-01-18 18:33:03 +0700

    Add files via upload
    ---- classes ----
    [+] Wrapper Classes
    [+] Exceptions
    [+] RegEx
    {+] Thread
    [+] Lambda

---
+++ src/dev/varaday/belajarjava/classes/Lambda.java
@@ -0,0 +1,62 @@
+package dev.varaday.belajarjava.classes;
+
+/* Ekspresi Lambda ditambahkan di Java 8.
+Ekspresi lambda adalah blok kode pendek yang mengambil parameter dan mengembalikan nilai.
+Ekspresi lambda mirip dengan metode, tetapi tidak memerlukan nama dan dapat diterapkan langsung di bagian isi metode.
+ */
+
+/* Menggunakan Lambda Expressions
+Ekspresi lambda biasanya diteruskan sebagai parameter ke suatu fungsi:
+ */
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+class L {
+    public static void main(String[] args) {
+        ArrayList<Integer> jumlah = new ArrayList<Integer>();
+        jumlah.add(5);
+        jumlah.add(9);
+        jumlah.add(8);
+        jumlah.add(1);
+        jumlah.forEach( (n) -> { System.out.println(n); } );
+    }
+}
+
+/* Ekspresi lambda dapat disimpan dalam variabel jika tipe variabelnya adalah antarmuka yang hanya memiliki satu metode.
+Ekspresi lambda harus memiliki jumlah parameter yang sama dan tipe kembalian yang sama dengan metode itu.
+Java memiliki banyak antarmuka bawaan seperti ini, seperti antarmuka Konsumen (ditemukan dalam paket java.util) yang digunakan oleh daftar.
+ */
+
+class L1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> jumlah = new ArrayList<Integer>();
+        jumlah.add(5);
+        jumlah.add(9);
+        jumlah.add(8);
+        jumlah.add(1);
+        Consumer<Integer> method = (n) -> { System.out.println(n); };
+        jumlah.forEach( method );
+    }
+}
+
+/* Untuk menggunakan ekspresi lambda dalam sebuah metode, metode tersebut harus memiliki parameter dengan antarmuka metode tunggal sebagai tipenya.
+Memanggil metode antarmuka akan menjalankan ekspresi lambda:
+ */
+
+interface StringFunction {
+    String run(String str);
+}
+
+class L3 {
+    public static void main(String[] args) {
+        StringFunction exclaim = (s) -> s + "!";
+        StringFunction ask = (s) -> s + "?";
+        printFormatted("Hello", exclaim);
+        printFormatted("Hello", ask);
+    }
+    public static void printFormatted(String str, StringFunction format) {
+        String result = format.run(str);
+        System.out.println(result);
+    }
+}
---
+++ src/dev/varaday/belajarjava/classes/RegEx.java
@@ -0,0 +1,52 @@
+package dev.varaday.belajarjava.classes;
+
+/* Java Regular Expressions
+Ekspresi reguler adalah urutan karakter yang membentuk pola pencarian.
+Saat Anda mencari data dalam teks, Anda dapat menggunakan pola pencarian ini untuk menjelaskan apa yang Anda cari.
+Ekspresi reguler dapat berupa satu karakter, atau pola yang lebih rumit.
+Ekspresi reguler dapat digunakan untuk melakukan semua jenis pencarian teks dan operasi penggantian teks.
+Java tidak memiliki kelas Ekspresi Reguler bawaan, tetapi kita dapat mengimpor paket java.util.regex untuk bekerja dengan ekspresi reguler.
+Paket tersebut mencakup kelas-kelas berikut:
+
+- Pattern Class - Mendefinisikan pola (untuk digunakan dalam pencarian)
+- Matcher Class - Digunakan untuk mencari pola
+- Pattern SyntaxException - Menunjukkan kesalahan sintaks dalam pola ekspresi reguler
+
+Contoh:
+ */
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class REx {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("Varaday", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher("Visit Varaday!");
+        boolean matchFound = matcher.find();
+        if(matchFound) {
+            System.out.println("Pertandingan ditemukan");
+        } else {
+            System.out.println("Pertandingan tidak ditemukan");
+        }
+    }
+}
+// Hasil yang Cocok ditemukan
+
+/* Contoh Dijelaskan
+Dalam contoh ini, Kata "Varaday" sedang dicari dalam sebuah kalimat.
+
+Pertama, pola dibuat menggunakan metode Pattern.compile().
+Parameter pertama menunjukkan pola mana yang sedang dicari dan parameter kedua memiliki tanda yang menunjukkan bahwa pencarian harus peka huruf besar kecil.
+Parameter kedua adalah opsional.
+Metode matcher() digunakan untuk mencari pola dalam sebuah string.
+Ini mengembalikan objek Matcher yang berisi informasi tentang pencarian yang dilakukan.
+Metode find() mengembalikan nilai true jika pola ditemukan dalam string dan false jika tidak ditemukan.
+ */
+
+/* Bendera
+Tanda dalam metode compile() mengubah cara pencarian dilakukan. Berikut ini beberapa di antaranya:
+
+- Pattern CASE_INSENSITIVE - Kapitalisasi huruf akan diabaikan saat melakukan pencarian.
+- Pattern LITERAL - Karakter khusus dalam pola tidak akan memiliki arti khusus dan akan diperlakukan sebagai karakter biasa saat melakukan pencarian.
+- Pattern UNICODE_CASE - Gunakan bersama dengan bendera CASE_INSENSITIVE untuk juga mengabaikan huruf besar / kecil di luar alfabet Inggris
+ */
